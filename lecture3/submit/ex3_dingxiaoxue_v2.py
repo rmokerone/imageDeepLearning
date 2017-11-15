d5ee7f5ab392f903dba4b356a1dd97b295fa72af
skaiu <rmokerone@gmail.com>
2017-11-15 20:20:17 +0800

    dingxiaoxue

--- lecture3/submit/ex3_dingxiaoxue_v2.py
+++ lecture3/submit/ex3_dingxiaoxue_v2.py
@@ -7,12 +7,22 @@ def load(filename):
     ret_X1 = []
     ret_X2 = []
     ret_y = []
+    a1=[]
+    a2=[]
     # ================== YOUR CODE HERE =============================
     for line in open(filename):
-        l=line.split()
-        ret_X1.append(float(l[0]))
-        ret_X2.append(float(l[1]))
-        ret_y.append(float(l[0])+float(l[1]))
+        l=line.split(' ')
+        a1.append(int(l[0]))
+        a2.append(int(l[1]))
+    s1=max(a1)-min(a1)
+    s2=max(a2)-min(a2)
+    a1aver=float(sum(a1)/len(a1))
+    a2aver=float(sum(a2)/len(a2))
+    m=len(a1)
+    for k in range(m):
+        ret_X1.append(float((a1[k]-a1aver)/s1))
+        ret_X2.append(float((a2[k]-a2aver)/s2))
+        ret_y.append(float((a1[k]-a1aver)/s1+(a2[k]-a2aver)/s2))
     # ===============================================================
     return ret_X1,ret_X2, ret_y
 
@@ -50,10 +60,10 @@ if __name__ == '__main__':
     theta1 = 0.0
     theta2 = 0.0
     iterations = 3000
-    alpha = 0.01
+    alpha = 0.03
     # ================ Part 1: Loading Data =========================
     print('Loading Data ...')
-    X1,X2, y = load('dataset_1_1.txt')
+    X1,X2, y = load('c:\\dataset_1_1.txt')
     m = len(y)
     print('m = %s' % m)
     print('X1[:10] = %s' % X1[:10])
@@ -62,24 +72,20 @@ if __name__ == '__main__':
 
     # ================ Part 2: Testing CostFuntion ==================
     print('Testing CostFuntion ...')
-    # compute initial cost
     J = computeCost(X1,X2, y, theta0, theta1,theta2)
     print('With theta0 = 0 and theta1 = 0 and theta2 = 0')
     print('Cost computed = %f' % J)
-    #print('Expected cost value (approx) 32.07')
-    # further testing of the cost function
     J = computeCost(X1,X2, y, 0.0, 1.0,1.0)
     print('with theta0 = 0 and theta1 = 1 and theta2 = 1')
     print('Cost computed = %f' % J)
-    #print('Expected cost value (approx) 54.24')
+
 
     # ================ Part 3: Gradient descent =====================
     print('Running Gradient Descent ...')
     theta0, theta1,theta2 = gradientDescent(X1,X2, y, theta0, theta1,theta2, alpha, iterations)
     print('Theta found by gradient descent:')
     print('theta0 = %f, theta1 = %f,theta3 = %f' % (theta0, theta1,theta2))
-    print('Expected theta values (approx)')
-    #print('theta0 = -3.6303, theta1 = 1.1664')
+   
 
     # ================ Part 4: Predict values =======================
     #predict1 = theta0 + 3.5 * theta1
